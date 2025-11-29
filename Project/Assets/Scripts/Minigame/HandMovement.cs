8534899fd763a5548fb03b2d5cb4ffc320e2f15b
Alzbeta Kulichova <kulichova.alzbeta@gmail.com>
2025-11-29 09:26:44 +0100

    Save before nuking the system and changing it for arrow keys movement

--- Project/Assets/Scripts/Minigame/HandMovement.cs
+++ Project/Assets/Scripts/Minigame/HandMovement.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class HandMovement : MonoBehaviour
 {
+    [Header("Hand movement")]
     public Vector2 Offset;
-    public State movementState;
+    public State MovementState;
     [Range(0f, 20f)]
-    public float followSpeed = 10f;
+    public float FollowSpeed = 10f;
+
+    [Header("Hand trembling")]
+    public float ShakingX = 1f;
+    public float ShakingY = 0.3f;
+    [Range(0.1f, 20f)]
+    public float ShakeSpeed = 5f;
+    private float shakeTimeX;
+    private float shakeTimeY;
+
+    [Header("Mouse drifting (ice)")]
+    public float DriftAcceleration = 10f;
+    public float DriftFriction = 3f;
+    private Vector3 driftVelocity;
+
 
 
     public enum State
     {
-        Following
+        FollowingMouse,
+        NotFollowingMouse
     }
 
     void Start()
@@ -20,23 +36,54 @@ public class HandMovement : MonoBehaviour
 
     void Update()
     {
-        switch (movementState)
+        switch (MovementState)
         {
-            case State.Following:
-                SetPositionByMouse();
+            case State.FollowingMouse:
+                SetPositionByMouse_org();
                 break;
             default:
                 break;
         }
     }
 
+    void SetPositionByMouse_org() 
+    { 
+        Vector2 mouseScreenPos = UnityEngine.InputSystem.Mouse.current.position.ReadValue(); 
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(
+            new Vector3(mouseScreenPos.x, mouseScreenPos.y, 0)
+        ); 
+        mouseWorldPos.z = 0; mouseWorldPos += (Vector3)Offset; 
+        transform.position = Vector3.Lerp(transform.position, mouseWorldPos + SetTrembling(), FollowSpeed * Time.deltaTime); 
+    }
+
     void SetPositionByMouse()
     {
         Vector2 mouseScreenPos = UnityEngine.InputSystem.Mouse.current.position.ReadValue();
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, 0));
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(
+            new Vector3(mouseScreenPos.x, mouseScreenPos.y, 0)
+        );
         mouseWorldPos.z = 0;
         mouseWorldPos += (Vector3)Offset;
 
-        transform.position = Vector3.Lerp(transform.position, mouseWorldPos, followSpeed * Time.deltaTime);
+        mouseWorldPos += SetTrembling();
+
+        Vector3 dir = mouseWorldPos - transform.position;
+        driftVelocity += dir * DriftAcceleration * Time.deltaTime;
+
+        driftVelocity -= driftVelocity * DriftFriction * Time.deltaTime;
+
+        transform.position += driftVelocity * Time.deltaTime;
+    }
+
+
+    Vector3 SetTrembling()
+    {
+        shakeTimeX += Time.deltaTime * ShakeSpeed;
+        shakeTimeY += Time.deltaTime * ShakeSpeed * 1.13f;
+
+        float x = Mathf.PerlinNoise(shakeTimeX, 0f) * 2f - 1f;
+        float y = Mathf.PerlinNoise(shakeTimeY, 0f) * 2f - 1f;
+
+        return new Vector3(x * ShakingX, y * ShakingY, 0f);
     }
 }
